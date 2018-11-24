151d70d795457ec4294d48a0f43a24f0a2178e91
Evan Pratten <ewpratten@gmail.com>
2018-11-24 17:26:05 -0500

    parser v1

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mpParser
 A tool for parsing motion profiling csv files into c++ headers
+
+## Usage
+Make sure python3 is installed, then run:
+```
+python3 mpParser/ ./path/to/file_csv_left.csv ./path/to/output/auto_name.h
+```
---
+++ mpParser/template.h
@@ -0,0 +1,16 @@
+#ifndef _{name}_HG_
+#define _{name}_HG_
+//Select the green highlighted cells and paste into  a csv file.
+//No need to copy the blank lines at the bottom.
+//This can be pasted into an array for direct use in C++/Java.
+//       Position (rotations)	Velocity (RPM)	Duration (ms)
+const int kMotionProfileSz ={lines};
+
+const double kMotionProfile[][3] = {
+{0,	0	,10},
+{data}
+	
+};
+
+
+#endif
